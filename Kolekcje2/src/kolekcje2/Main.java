b0877c3d180c78294272cd8336695f4b8797d8bc
Dreamcatcher94 <b1965501@trbvn.com>
2016-03-15 05:08:03 +0100

    commit
    commit

---
+++ Kolekcje2/src/kolekcje2/Main.java
@@ -0,0 +1,15 @@
+package kolekcje2;
+
+
+/**
+ * Created by Marta Pawlik on 13.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        new kolekcje2.KsiazkaAdresowa();
+
+    }
+
+    }
---
+++ Kolekcje3/src/kolekcje3/Kolekcje3.java
@@ -0,0 +1,13 @@
+package kolekcje3;
+
+/**
+ * Created by Marta Pawlik on 13.02.2016.
+ */
+
+public class Kolekcje3 {
+    
+    public static void main(String[] args) {
+        TestLists test = new TestLists();
+    }
+
+}
